bf489b450e41bf9a1ad4ca3f04965371bc7cff0b
Andy Song <sczhoustill@126.com>
2024-12-08 19:20:50 +0800

    112

--- projects/app/src/service/common/system/index.ts
+++ projects/app/src/service/common/system/index.ts
@@ -63,8 +63,7 @@ const defaultFeConfigs: FastGPTFeConfigsType = {
   appTemplateCourse:
     'https://fael3z0zfze.feishu.cn/wiki/CX9wwMGyEi5TL6koiLYcg7U0nWb?fromScene=spaceOverview',
   systemTitle: 'WudaoAI',
-  concatMd:
-    '项目开源地址: [FastGPT GitHub](https://github.com/labring/FastGPT)\n交流群: ![](https://oss.laf.run/otnvvf-imgs/fastgpt-feishu1.png)',
+  concatMd: '',
   limit: {
     exportDatasetLimitMinutes: 0,
     websiteSyncLimitMinuted: 0
