eea9d589a4dd5c9830c342a8db85200e19176ce9
Andy Song <sczhoustill@126.com>
2025-02-25 11:10:54 +0800

    4、show_git: false

--- projects/app/src/service/common/system/index.ts
+++ projects/app/src/service/common/system/index.ts
@@ -51,7 +51,7 @@ export async function getInitConfig() {
 
 const defaultFeConfigs: FastGPTFeConfigsType = {
   show_emptyChat: true,
-  show_git: true,
+  show_git: false,
   docUrl: 'https://www.5d321.com',
   openAPIDocUrl: 'https://doc.tryfastgpt.ai/docs/development/openapi',
   systemPluginCourseUrl: 'https://fael3z0zfze.feishu.cn/wiki/ERZnw9R26iRRG0kXZRec6WL9nwh',
